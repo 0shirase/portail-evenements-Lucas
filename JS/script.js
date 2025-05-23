af22c888fbd43fac373e66c9eda42ac210606cf1
0shirase <lucas0999.lb@gmail.com>
2025-05-23 16:41:05 +0200

    FEAT: ajout d'animations sur les étoiles aux cliques sur les étoiles

--- JS/script.js
+++ JS/script.js
@@ -6,38 +6,39 @@ import { setCookie, getCookie } from "./cookies.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const events = await getEvents();
   const container = document.getElementById("event-list");
+  /* console.log(events); */
 
   //Event creation
   events.forEach((event) => {
+    //createElement cards
     const eventElement = document.createElement("article");
+    const eventHeader = document.createElement("div");
+    const eventTitle = document.createElement("h3");
+    const favStar = document.createElement("i");
+    const eventDate = document.createElement("p");
+    const eventLocation = document.createElement("p");
+
+    //classList add
     eventElement.classList.add("event");
+    eventHeader.classList.add("event-header");
 
-    // Header
-    const header = document.createElement("div");
-    header.classList.add("event-header");
-
-    const title = document.createElement("h3");
-    title.textContent = event.title;
-
-    const star = document.createElement("i");
-    star.className = "fa-solid fa-star favorite-icon";
-    star.dataset.id = event.id;
-    star.title = "Ajouter à mon planning";
-
-    header.appendChild(title);
-    header.appendChild(star);
-
-    // Date
-    const date = document.createElement("p");
-    date.innerHTML = `<p>Date :</p> ${new Date(
+    //textContent
+    eventTitle.textContent = event.title;
+    eventDate.textContent = ` Date et heure: ${new Date(
       event.start_date
     ).toLocaleString()}`;
-
-    // Lieu
-    const location = document.createElement("p");
-    location.innerHTML = `<p>Lieu :</p> ${
+    eventLocation.textContent = ` Lieu: ${
       event.venue?.address || "Lieu non précisé"
     }`;
+
+    //fav event
+    favStar.className = "fa-regular fa-star favorite-icon";
+    favStar.dataset.id = event.id;
+    favStar.title = "Ajouter à mon planning";
+
+    //appendChild stars
+    eventHeader.appendChild(eventTitle);
+    eventHeader.appendChild(favStar);
 
     // Button open Modal
     const button = document.createElement("button");
@@ -48,17 +49,39 @@ document.addEventListener("DOMContentLoaded", async () => {
     button.addEventListener("click", () => openModal(event));
 
     // appendChild
-    eventElement.appendChild(header);
-    eventElement.appendChild(date);
-    eventElement.appendChild(location);
+    eventElement.appendChild(eventHeader);
+    eventElement.appendChild(eventDate);
+    eventElement.appendChild(eventLocation);
     eventElement.appendChild(button);
-
     container.appendChild(eventElement);
+
+    //favStar animation
+    favStar.addEventListener("click", () => {
+      favStar.classList.add("vibrate");
+
+      for (let i = 0; i < 7; i++) {
+        const miniStar = document.createElement("i");
+        miniStar.className = "fa-solid fa-star mini-star";
+
+        favStar.appendChild(miniStar);
+
+        miniStar.addEventListener("animationend", () => {
+          miniStar.remove();
+        });
+      }
+
+      favStar.addEventListener(
+        "animationend",
+        () => {
+          favStar.classList.remove("vibrate");
+        },
+        { once: true }
+      );
+    });
   });
 
   //functions listener call
   setupModalListeners();
-  console.log(getCookie);
 
   const themeButton = document.getElementById("theme");
   const body = document.body;
